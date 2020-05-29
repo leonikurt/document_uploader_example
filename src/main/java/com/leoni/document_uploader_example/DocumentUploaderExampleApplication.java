9951b9206bfc3aa6bfcb5c0e8a2725ee2382fa61
Leoni Kurt <leonikurt@gmail.com>
2020-05-29 19:47:28 -0300

    Initializing project and DB Connection in application.properties

---
+++ src/main/java/com/leoni/document_uploader_example/DocumentUploaderExampleApplication.java
@@ -0,0 +1,13 @@
+package com.leoni.document_uploader_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentUploaderExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentUploaderExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leoni/document_uploader_example/DocumentUploaderExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoni.document_uploader_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentUploaderExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
